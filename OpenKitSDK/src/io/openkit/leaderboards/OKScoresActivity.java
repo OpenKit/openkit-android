d9262adb9e92b3f30a9fc20cc667d2e26957fd59
Suneet Shah <suneets@gmail.com>
2013-04-17 18:33:48 -0700

    removed unused import

--- OpenKitSDK/src/io/openkit/leaderboards/OKScoresActivity.java
+++ OpenKitSDK/src/io/openkit/leaderboards/OKScoresActivity.java
@@ -18,7 +18,6 @@ package io.openkit.leaderboards;
 
 import io.openkit.*;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.MenuItem;
