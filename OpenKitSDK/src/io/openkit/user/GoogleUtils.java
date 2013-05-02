154a838aac8f1ab7219f1cfcbf25d29b10a87486
Suneet Shah <suneets@gmail.com>
2013-05-01 18:44:59 -0700

    added more utility methods

--- OpenKitSDK/src/io/openkit/user/GoogleUtils.java
+++ OpenKitSDK/src/io/openkit/user/GoogleUtils.java
@@ -1,84 +1,100 @@
 package io.openkit.user;
 
-import java.io.IOException;
+
+import io.openkit.asynchttp.AsyncHttpClient;
+import io.openkit.asynchttp.OKJsonHttpResponseHandler;
+import io.openkit.asynchttp.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.util.Log;
 
-import com.google.android.gms.auth.GoogleAuthException;
-import com.google.android.gms.auth.GoogleAuthUtil;
-import com.google.android.gms.auth.UserRecoverableAuthException;
-
 public class GoogleUtils {
-	
+
 	private static final String SCOPE = "oauth2:https://www.googleapis.com/auth/userinfo.profile";
-	
-	public static String[] getAccountNames(Context ctx) 
+
+	static final int REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR = 1002;
+
+	private static void logit(String message){
+		Log.d("OKC", message);
+	}
+
+	public static String[] getGoogleAccountNames(Context ctx) 
 	{
-        AccountManager manager = AccountManager.get(ctx);
-        Account[] accounts = manager.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
-        String[] names = new String[accounts.length];
-        for (int i = 0; i < names.length; i++) {
-            names[i] = accounts[i].name;
-        }
-        return names;
-    }
-	
-	private static Account getAccount(Context ctx) {
+		Account[] accounts = getGoogleAccounts(ctx);
+		String[] names = new String[accounts.length];
+		for (int i = 0; i < names.length; i++) {
+			names[i] = accounts[i].name;
+		}
+		return names;
+	}
+
+	public static Account[] getGoogleAccounts(Context ctx)
+	{
 		AccountManager manager  = AccountManager.get(ctx);
-        Account[] accounts = manager.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
-        return accounts[0];
-    }
-	
-	public static void getAccountAuthToken(final Activity activity)
-    {
-    	AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+		return manager.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+	}
+
+	public static void showGooglePlayServicesErrorDialog(final Activity activityToShowFrom, final int code) {
+		activityToShowFrom.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Dialog d = GooglePlayServicesUtil.getErrorDialog(
+						code,
+						activityToShowFrom,
+						REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
+				d.show();
+			}
+		});
+	}
+
+	public static void getGoogleUserInfo(String authToken)
+	{
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.addHeader("Content-Type", "application/json");
+		client.addHeader("Accept", "application/json");
+
+		RequestParams params =  new RequestParams();
+		params.put("access_token", authToken);
+
+		client.get("https://www.googleapis.com/oauth2/v1/userinfo", params, new OKJsonHttpResponseHandler() {
 
 			@Override
-			protected Void doInBackground(Void... params) {
-				getAccountAuthTokenBlocking(activity);
-				return null;
+			public void onSuccess(JSONObject object) {
+				// TODO Auto-generated method stub
+				logit( "Got json object: " + object);
 			}
-			
+
 			@Override
-			protected void onPostExecute(Void result)
-			{
-				Log.d("TestGoog", "Finished async task");
+			public void onSuccess(JSONArray array) {
+				logit( "Got json array: " + array);				
 			}
-		};
-		
-		task.execute();
-    }
-	
-	public static void getAccountAuthTokenBlocking(Activity activity)
-    {
-    	Account account = getAccount(activity);
-    	
-    	try 
-    	{
-			String token = GoogleAuthUtil.getToken(activity, account.name, SCOPE);
-			Log.d("TestGoog", "Got auth token: " + token);
-		} 
-    	catch (UserRecoverableAuthException userException) 
-    	{
-			// Unable to authenticate, but the user can fix this.
-			// Forward the user to the appropriate activity.
-    		activity.startActivityForResult(userException.getIntent(), 53);
-	        userException.printStackTrace();
-		} 
-    	catch (IOException e) 
-    	{	
-			e.printStackTrace();
-		} 
-    	catch (GoogleAuthException e) 
-    	{
-			Log.d("TestGoog", "Unrecoverable error: " + e.getMessage());
-			e.printStackTrace();
-		}
-    }
+
+			@Override
+			public void onFailure(Throwable error, String content) {
+				logit( "Error: " + error + "content: " + content);
+			}
+
+			@Override
+			public void onFailure(Throwable e, JSONArray errorResponse) {
+				logit( "Error: " + e);
+			}
+
+			@Override
+			public void onFailure(Throwable e, JSONObject errorResponse) {
+				logit( "Error: " + e);
+			}
+		});
+	}
+
 
 }
