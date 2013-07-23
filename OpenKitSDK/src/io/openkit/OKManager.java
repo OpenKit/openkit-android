058e1e64c0909c6cecd2b6c3d2ab8b8d602384f7
Suneet Shah <suneets@gmail.com>
2013-07-22 19:15:00 -0700

    added get/set for secretKey and appKey and updated UnityPlugin

--- OpenKitSDK/src/io/openkit/OKManager.java
+++ OpenKitSDK/src/io/openkit/OKManager.java
@@ -21,20 +21,20 @@ import android.content.SharedPreferences;
 import io.openkit.facebook.Session;
 
 /**
- * Singleton instance that stores OpenKit global items 
- * such as the current user and OpenKit application. This should not be 
- * accessed directly. 
+ * Singleton instance that stores OpenKit global items
+ * such as the current user and OpenKit application. This should not be
+ * accessed directly.
  * @author suneets
  *
  */
 
 public enum OKManager {
 	INSTANCE;
-	
+
 	private OKUser currentUser;
 	private String appKey;
 	private String secretKey;
-	
+
 	/**
 	 * OpenKit internal only, developers should use OpenKit.initialize()
 	 * @param context
@@ -45,9 +45,14 @@ public enum OKManager {
 	{
 		this.appKey = appKey;
 		this.secretKey = secretKey;
+		initialize(context);
+	}
+
+	public void initialize(Context context)
+	{
 		this.getCurrentUser(context);
 	}
-	
+
 	/**
 	 * @return Returns the OpenKit application key.
 	 */
@@ -55,17 +60,28 @@ public enum OKManager {
 	{
 		return this.appKey;
 	}
-	
+
+	public void setAppKey(String appKey)
+	{
+		this.appKey = appKey;
+	}
+
+
 	public OKUser getCurrentUser()
 	{
 		return currentUser;
 	}
-	
+
 	public String getSecretKey()
 	{
 		return secretKey;
 	}
-	
+
+	public void setSecretKey(String secretKey)
+	{
+		this.secretKey = secretKey;
+	}
+
 	/**
 	 * Get current user from shared preferences if stored
 	 * @param Context, required to pull current user stored in SharedPreferences
@@ -80,7 +96,7 @@ public enum OKManager {
 			return getOKUserInSharedPrefs(ctx);
 		}
 	}
-	
+
 	/**
 	 * OpenKit internal only. Must call this method when a user logs in with OpenKit.
 	 * @param aUser User
@@ -91,7 +107,7 @@ public enum OKManager {
 		this.currentUser = aUser;
 		saveOKUserInSharedPrefs(context, currentUser);
 	}
-	
+
 	/**
 	 * Log the current user out of OpenKit and delete all cached/stored data associated with the user. Requires a context.
 	 * @param context
@@ -104,54 +120,54 @@ public enum OKManager {
 		if(session != null)
 			session.closeAndClearTokenInformation();
 	}
-	
+
 	/**
-	 * Constants used for saving and retrieving OKUser in user preferences 
+	 * Constants used for saving and retrieving OKUser in user preferences
 	 */
-	
+
 	private static final String OK_PREFS_NAME = "openkit_pref";
 	private static final String KEY_fbUserId = "FBUserID";
 	private static final String KEY_twitterUserID = "twitterUserID";
 	private static final String KEY_userNick = "userNick";
 	private static final String KEY_okUserID = "OKuserID";
-	
-	
+
+
 	private void saveOKUserInSharedPrefs(Context ctx, OKUser user)
 	{
 		SharedPreferences settings = ctx.getSharedPreferences(OK_PREFS_NAME, Context.MODE_PRIVATE);
 		SharedPreferences.Editor editor = settings.edit();
-		
+
 		editor.putString(KEY_userNick, user.getUserNick());
 		editor.putInt(KEY_okUserID, user.getOKUserID());
 		editor.putLong(KEY_fbUserId, user.getFBUserID());
 		editor.putLong(KEY_twitterUserID, user.getTwitterUserID());
-		
+
 		editor.commit();
 	}
-	
+
 	private void deleteUserInSharedPrefs(Context ctx)
 	{
 		SharedPreferences settings = ctx.getSharedPreferences(OK_PREFS_NAME, Context.MODE_PRIVATE);
 		SharedPreferences.Editor editor = settings.edit();
-		
+
 		editor.remove(KEY_userNick);
 		editor.remove(KEY_okUserID);
 		editor.remove(KEY_fbUserId);
 		editor.remove(KEY_twitterUserID);
-		
+
 		editor.commit();
 	}
-	
+
 	private OKUser getOKUserInSharedPrefs(Context ctx)
 	{
 		SharedPreferences settings = ctx.getSharedPreferences(OK_PREFS_NAME, Context.MODE_PRIVATE);
 		OKUser user = new OKUser();
-		
+
 		user.setFBUserID(settings.getLong(KEY_fbUserId, 0));
 		user.setOKUserID(settings.getInt(KEY_okUserID, 0));
 		user.setTwitterUserID(settings.getLong(KEY_twitterUserID, 0));
 		user.setUserNick(settings.getString(KEY_userNick, null));
-		
+
 		if(user.getOKUserID() == 0)
 			return null;
 		else
