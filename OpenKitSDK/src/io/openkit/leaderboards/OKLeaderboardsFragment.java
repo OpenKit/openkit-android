9304f59a9a17cc70830d845a856b2e7966fd84f4
Suneet Shah <suneets@gmail.com>
2013-08-20 18:58:49 -0700

    remove unused code

--- OpenKitSDK/src/io/openkit/leaderboards/OKLeaderboardsFragment.java
+++ OpenKitSDK/src/io/openkit/leaderboards/OKLeaderboardsFragment.java
@@ -55,20 +55,16 @@ public class OKLeaderboardsFragment extends ListFragment {
 		// Note: have to lookup id manually for Unity
 		int viewID = getResources().getIdentifier("io_openkit_fragment_leaderboards", "layout", getActivity().getPackageName());
 		View view = inflater.inflate(viewID, container, false);
-		//View view = inflater.inflate(R.layout.io_openkit_fragment_leaderboards, container, false);
 
 		int spinnerBarID = getResources().getIdentifier("progressSpinner", "id", getActivity().getPackageName());
 		int listHeaderViewID = getResources().getIdentifier("list_simple_header", "layout", getActivity().getPackageName());
 		int listHeaderTextViewID = getResources().getIdentifier("headerTextView", "id", getActivity().getPackageName());
 
 		listView = (ListView)view.findViewById(android.R.id.list);
-		//spinnerBar = (ProgressBar)view.findViewById(R.id.progressSpinner);
 		spinnerBar = (ProgressBar)view.findViewById(spinnerBarID);
 
-		//Inflate the list headerview
-		//View listHeaderView = inflater.inflate(R.layout.list_simple_header, null);
+
 		View listHeaderView = inflater.inflate(listHeaderViewID, null);
-		//listHeaderTextView = (TextView)listHeaderView.findViewById(R.id.headerTextView);
 		listHeaderTextView = (TextView)listHeaderView.findViewById(listHeaderTextViewID);
 		listView.addHeaderView(listHeaderView);
 
